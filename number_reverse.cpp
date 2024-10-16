c4c1c3c90650b9ac11efd7ac2131141c9a0ff183
gargipedhekar <gargi27.pedhekar@gmail.com>
2024-10-16 15:24:23 +0530

    Create number_reverse.cpp

---
+++ number_reverse.cpp
@@ -0,0 +1,28 @@
+//Name:Gargi
+//PRN:23070123052
+//Aim:Implemention of Recursion to find Reverse of given Number
+
+#include <iostream>
+using namespace std;
+
+void rev(int i) {
+    if (i > 0) {
+        cout << (i % 10); 
+        rev(i / 10);      
+    }
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "Reversed number is: ";
+    rev(num);
+    cout << endl;
+}
+
+
+/*
+Enter a number: 20
+Reversed number is: 02
+*/
